3e07e805a216231d0293d586cc8fb10fc0b5832a
unknown <FS417600@STOFA-101-01.fss.is>
2015-11-11 08:46:02 +0000

    byrjað á login forminu

--- js/controllers/LoginController.js
+++ js/controllers/LoginController.js
@@ -2,5 +2,9 @@ app.controller('LoginController', ['$scope','login',
 function($scope, login) {
   login.success(function(data) {
     $scope.login = data;
+
+    $scope.Submit = function(){
+      alert("hallo");
+    }
   });
 }]);
