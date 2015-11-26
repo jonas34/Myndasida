a3ac812dde7946dab2b2625f6a87f9714de3d670
unknown <FS417600@stofa-110-1.fss.is>
2015-11-26 09:53:17 +0000

    Tók login viewið frá Brynjari og setti það inn hjá mér

--- lokaverkefni/myndir/urls.py
+++ lokaverkefni/myndir/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^register/', views.register, name='register'),
+    url(r'^login/',views.log_in,name='login'),
     url(r'^photos/', views.photos, name='photos'),
     #url(r'^comments/', views.comments, name='comments'),
 ]
--- lokaverkefni/myndir/views.py
+++ lokaverkefni/myndir/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import MyUser, Photos
 from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.core import serializers
+from django.conf import settings
 
 
 # Create your views here.
@@ -18,6 +20,19 @@ def register(request):
     u = MyUser.objects.create_user(email, dateofbirth, password)
     u.save()
     return HttpResponse("you are in register")
+
+def log_in(request):
+    if not request.user.is_authenticated():
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+    user_name=request.GET['Username']
+    passw = request.GET['Password']
+    user = authenticate(email=user_name, password=passw)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return HttpResponse("login successful")
+        else: return HttpResponse("disabled account")
+    else: return HttpResponse("invalid login")
 
 def photos(request):
     photos = Photos.objects.all()
