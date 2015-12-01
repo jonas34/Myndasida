3f3b24a2afdd48ef9f343ed4e7af6ebf25ed92f0
unknown <FS417600@STOFA-101-01.fss.is>
2015-12-01 08:49:45 +0000

    getur núna postað myndum í gegnum admin síðuna

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,6 +1,7 @@
 app.controller('MainController', ['$scope', 'photos', function($scope, photos) {
   photos.success(function(data) {
+    console.log(data);
     $scope.myndir = data;
-    document.body.style.background = "#ffffff";
+    document.body.style.background = "#c3dfef";
   });
 }]);
